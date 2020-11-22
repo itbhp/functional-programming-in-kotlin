c3b34cb3d4228d2bf5c73ae97d820e9033c2b821
Paolo Sciarra <itbhps@gmail.com>
2020-11-22 19:12:41 +0100

    implemented maximum

--- src/main/kotlin/it/twinsbrains/chapter3/Tree.kt
+++ src/main/kotlin/it/twinsbrains/chapter3/Tree.kt
@@ -1,11 +1,19 @@
 package it.twinsbrains.chapter3
 
+import kotlin.math.max
+
 sealed class Tree<out A> {
   companion object {
     fun <A> size(tree: Tree<A>): Int =
       when (tree) {
         is Leaf -> 1
         is Branch -> 1 + size(tree.left) + size(tree.right)
+      }
+
+    fun maximum(tree: Tree<Int>): Int =
+      when (tree) {
+        is Leaf -> tree.value
+        is Branch -> max(maximum(tree.left), maximum(tree.right))
       }
   }
 }
--- src/test/kotlin/it/twinsbrains/chapter3/TreeTest.kt
+++ src/test/kotlin/it/twinsbrains/chapter3/TreeTest.kt
@@ -2,6 +2,7 @@ package it.twinsbrains.chapter3
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import it.twinsbrains.chapter3.Tree.Companion.maximum
 import it.twinsbrains.chapter3.Tree.Companion.size
 import org.junit.Test
 
@@ -10,5 +11,11 @@ class TreeTest {
   fun sizeTest() {
     val tree = Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))
     assertThat(size(tree)).isEqualTo(5)
+  }
+
+  @Test
+  fun maximumTest() {
+    val tree = Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))
+    assertThat(maximum(tree)).isEqualTo(3)
   }
 }
