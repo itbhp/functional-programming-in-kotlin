9a2459a4a7c7af9a5b0f41473d7da247d138056d
Paolo Sciarra <itbhps@gmail.com>
2020-12-04 17:45:10 +0100

    implemented map2 on Rand

--- src/main/kotlin/it/twinsbrains/fpik/chapter6/Rand.kt
+++ src/main/kotlin/it/twinsbrains/fpik/chapter6/Rand.kt
@@ -1,20 +1,37 @@
 package it.twinsbrains.fpik.chapter6
 
+import it.twinsbrains.fpik.chapter6.RandCompanion.map
+import it.twinsbrains.fpik.chapter6.RandCompanion.map2
+import it.twinsbrains.fpik.chapter6.RandCompanion.unit
+
+typealias Rand<A> = (RNG) -> Pair<A, RNG>
+
 interface RNG {
     fun nextInt(): Pair<Int, RNG>
 }
 
-typealias Rand<A> = (RNG) -> Pair<A, RNG>
+object RandCompanion {
+    fun <A> unit(a: A): Rand<A> = { rng -> Pair(a, rng) }
 
-fun <A> unit(a: A): Rand<A> = { rng -> Pair(a, rng) }
+    fun <A, B> map(s: Rand<A>, f: (A) -> B): Rand<B> = { rng ->
+        val (a, rng2) = s(rng)
+        Pair(f(a), rng2)
+    }
 
-fun <A, B> map(s: Rand<A>, f: (A) -> B): Rand<B> = { rng ->
-    val (a, rng2) = s(rng)
-    Pair(f(a), rng2)
+    fun <A, B, C> map2(
+        ra: Rand<A>,
+        rb: Rand<B>,
+        f: (A, B) -> C
+    ): Rand<C> = { rng: RNG ->
+        val (n1, rng1) = ra(rng)
+        val (n2, rng2) = rb(rng1)
+        f(n1, n2) to rng2
+    }
 }
 
 object RandExamples {
-    val intR: Rand<Int> = { rng -> rng.nextInt() }
-    val nonNegativeIntR: Rand<Int> = map(intR) { if (it < 0) -(it + 1) else it }
+    private val intR: Rand<Int> = unit(3)
+    private val nonNegativeIntR: Rand<Int> = map(intR) { if (it < 0) -(it + 1) else it }
     val doubleR: Rand<Double> = map(nonNegativeIntR) { it.toDouble() / Int.MAX_VALUE }
+    val intDoubleR: Rand<Pair<Int, Double>> = map2(nonNegativeIntR, doubleR) { a, b -> a to b }
 }
---
+++ src/test/kotlin/it/twinsbrains/fpik/chapter6/IntDoublesTest.kt
@@ -0,0 +1,26 @@
+package it.twinsbrains.fpik.chapter6
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.property.forAll
+import it.twinsbrains.fpik.chapter6.RandExamples.intDoubleR
+import it.twinsbrains.fpik.chapter6.LinearCongruentialGenerator as SimpleRNG
+
+class IntDoublesTest : StringSpec() {
+    init {
+        "int from the pair should be positives " {
+            forAll<Long> { seed ->
+                val (p, _) = intDoubleR(SimpleRNG(seed))
+                val (i, _) = p
+                i >= 0
+            }
+        }
+
+        "double from the pair should be in [0,1) " {
+            forAll<Long> { seed ->
+                val (p, _) = intDoubleR(SimpleRNG(seed))
+                val (_, d) = p
+                d >= 0 && d < 1
+            }
+        }
+    }
+}
