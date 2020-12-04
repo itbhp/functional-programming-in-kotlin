e4ed3c689d89d4046b8d17592e9da138f5218b96
Paolo Sciarra <itbhps@gmail.com>
2020-12-04 21:41:56 +0100

    added example using State.fx comprehension from arrow

---
+++ src/test/kotlin/it/twinsbrains/fpik/chapter6/StateMonadComprehensionTest.kt
@@ -0,0 +1,35 @@
+package it.twinsbrains.fpik.chapter6
+
+import arrow.core.Id
+import arrow.core.extensions.id.monad.monad
+import arrow.mtl.State
+import arrow.mtl.extensions.fx
+import arrow.mtl.run
+import it.twinsbrains.fpik.chapter6.StateMonadComprehension.int
+import it.twinsbrains.fpik.chapter6.StateMonadComprehension.ints
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.containsExactly
+import strikt.assertions.isEqualTo
+
+class StateMonadComprehensionTest {
+    @Test
+    fun `state monad comprehension`() {
+        val ns2: State<RNG, List<Int>> = State.fx(Id.monad()) {
+            val x: Int = int.bind()
+            val y: Int = int.bind()
+            val xs: List<Int> = ints(x).bind()
+            xs.map { it % y }
+        }
+
+        val listInt = ns2.run(Fixed(2)).b
+
+        expectThat(listInt.size).isEqualTo(2)
+    }
+}
+
+class Fixed(private val n: Int) : RNG {
+    override fun nextInt(): Pair<Int, RNG> {
+        return n to this
+    }
+}
