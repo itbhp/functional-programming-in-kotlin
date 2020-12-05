e100ceaa432f61e0b46eb7a15d538da6655aaf88
Paolo Sciarra <itbhps@gmail.com>
2020-12-05 12:51:26 +0100

    refactoring using state modify combinator

--- src/main/kotlin/it/twinsbrains/fpik/chapter6/CandyMachine.kt
+++ src/main/kotlin/it/twinsbrains/fpik/chapter6/CandyMachine.kt
@@ -2,6 +2,8 @@ package it.twinsbrains.fpik.chapter6
 
 import arrow.core.Tuple2
 import arrow.mtl.State
+import arrow.mtl.run
+import it.twinsbrains.fpik.chapter6.ArrowStateMonad.modify
 
 object CandyMachine {
     sealed class Input {
@@ -15,28 +17,25 @@ object CandyMachine {
         val coins: Int
     )
 
+    private fun transform(
+        machine: Machine,
+        input: Input
+    ): Machine {
+        return when {
+            input is Input.Turn && !machine.locked && machine.candies > 0 ->
+                Machine(true, machine.candies - 1, machine.coins)
+            input is Input.Coin && machine.locked && machine.candies > 0 ->
+                Machine(false, machine.candies, machine.coins + 1)
+            else -> machine
+        }
+    }
+
     fun simulateMachine(
         inputs: List<Input>
     ): State<Machine, Tuple2<Int, Int>> = State { machine ->
-        val lastMachine = inputs.fold(machine) { curMachine, i ->
-            val (newMachine, _) = onAnInput(curMachine, i)
-            newMachine
-        }
-        transitionTo(lastMachine)
-    }
-
-    private fun onAnInput(
-        machine: Machine,
-        input: Input
-    ): Tuple2<Machine, Tuple2<Int, Int>> {
-        val doNothing = transitionTo(machine)
-        return when {
-            input is Input.Turn && !machine.locked && machine.candies > 0 ->
-                transitionTo(Machine(true, machine.candies - 1, machine.coins))
-            input is Input.Coin && machine.locked && machine.candies > 0 ->
-                transitionTo(Machine(false, machine.candies, machine.coins + 1))
-            else -> doNothing
-        }
+        transitionTo(inputs.map { i ->
+            modify<Machine> { s -> transform(s, i) }
+        }.fold(machine) { cur, s -> s.run(cur).a })
     }
 
     private fun transitionTo(machine: Machine) =
