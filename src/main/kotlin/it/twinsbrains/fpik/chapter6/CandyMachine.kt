812a137aa19ab73e118128fd9aedae7f49173802
Paolo Sciarra <itbhps@gmail.com>
2020-12-05 12:34:28 +0100

    solved simulate candy machine exercise

---
+++ src/main/kotlin/it/twinsbrains/fpik/chapter6/CandyMachine.kt
@@ -0,0 +1,45 @@
+package it.twinsbrains.fpik.chapter6
+
+import arrow.core.Tuple2
+import arrow.mtl.State
+
+object CandyMachine {
+    sealed class Input {
+        object Coin : Input()
+        object Turn : Input()
+    }
+
+    data class Machine(
+        val locked: Boolean,
+        val candies: Int,
+        val coins: Int
+    )
+
+    fun simulateMachine(
+        inputs: List<Input>
+    ): State<Machine, Tuple2<Int, Int>> = State { machine ->
+        val lastMachine = inputs.fold(machine) { curMachine, i ->
+            val (newMachine, _) = onAnInput(curMachine, i)
+            newMachine
+        }
+        transitionTo(lastMachine)
+    }
+
+    private fun onAnInput(
+        machine: Machine,
+        input: Input
+    ): Tuple2<Machine, Tuple2<Int, Int>> {
+        val doNothing = transitionTo(machine)
+        return when {
+            machine.candies == 0 -> doNothing
+            input is Input.Turn && machine.locked -> doNothing
+            input is Input.Coin && !machine.locked -> doNothing
+            input is Input.Turn -> transitionTo(Machine(true, machine.candies - 1, machine.coins))
+            input is Input.Coin -> transitionTo(Machine(false, machine.candies, machine.coins + 1))
+            else -> doNothing
+        }
+    }
+
+    private fun transitionTo(machine: Machine) =
+        Tuple2(machine, Tuple2(machine.candies, machine.coins))
+}
---
+++ src/test/kotlin/it/twinsbrains/fpik/chapter6/CandyMachineTest.kt
@@ -0,0 +1,85 @@
+package it.twinsbrains.fpik.chapter6
+
+import arrow.core.Tuple2
+import arrow.mtl.State
+import arrow.mtl.run
+import it.twinsbrains.fpik.chapter6.CandyMachine.Input
+import it.twinsbrains.fpik.chapter6.CandyMachine.Input.Coin
+import it.twinsbrains.fpik.chapter6.CandyMachine.Input.Turn
+import it.twinsbrains.fpik.chapter6.CandyMachine.Machine
+import it.twinsbrains.fpik.chapter6.CandyMachine.simulateMachine
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+class CandyMachineTest {
+    @Test
+    fun `inserting a coin on out of candy machine`() {
+        val inputs = listOf<Input>(Coin)
+        val simulateMachine: State<Machine, Tuple2<Int, Int>> = simulateMachine(inputs)
+        val initialMachine = Machine(true, 0, 1)
+        val (newMachine, _) = simulateMachine.run(initialMachine)
+
+        expectThat(newMachine).isEqualTo(initialMachine)
+    }
+
+    @Test
+    fun `trying to turn the knob on an out of candy machine`() {
+        val inputs = listOf<Input>(Turn)
+        val simulateMachine: State<Machine, Tuple2<Int, Int>> = simulateMachine(inputs)
+        val initialMachine = Machine(false, 0, 1)
+        val (newMachine, _) = simulateMachine.run(initialMachine)
+
+        expectThat(newMachine).isEqualTo(initialMachine)
+    }
+
+    @Test
+    fun `turning the knob on locked machine`() {
+        val initialMachine = Machine(true, 1, 1)
+        val inputs = listOf<Input>(Turn)
+        val simulateMachine: State<Machine, Tuple2<Int, Int>> = simulateMachine(inputs)
+        val (newMachine, _) = simulateMachine.run(initialMachine)
+
+        expectThat(newMachine).isEqualTo(initialMachine)
+    }
+
+    @Test
+    fun `inserting a coin on unlocked machine`() {
+        val initialMachine = Machine(false, 1, 1)
+        val inputs = listOf<Input>(Coin)
+        val simulateMachine: State<Machine, Tuple2<Int, Int>> = simulateMachine(inputs)
+        val (newMachine, _) = simulateMachine.run(initialMachine)
+
+        expectThat(newMachine).isEqualTo(initialMachine)
+    }
+
+    @Test
+    fun `turning the knob on unlocked machine with candies`() {
+        val initialMachine = Machine(false, 1, 1)
+        val inputs = listOf<Input>(Turn)
+        val simulateMachine: State<Machine, Tuple2<Int, Int>> = simulateMachine(inputs)
+        val (newMachine, _) = simulateMachine.run(initialMachine)
+
+        expectThat(newMachine).isEqualTo(Machine(true, 0, 1))
+    }
+
+    @Test
+    fun `inserting coin on locked machine with candies`() {
+        val initialMachine = Machine(true, 1, 1)
+        val inputs = listOf<Input>(Coin)
+        val simulateMachine: State<Machine, Tuple2<Int, Int>> = simulateMachine(inputs)
+        val (newMachine, _) = simulateMachine.run(initialMachine)
+
+        expectThat(newMachine).isEqualTo(Machine(false, 1, 2))
+    }
+
+    @Test
+    fun `inserting coin on locked machine with candies and then turn the knob`() {
+        val initialMachine = Machine(true, 2, 1)
+        val inputs = listOf(Coin, Turn)
+        val simulateMachine: State<Machine, Tuple2<Int, Int>> = simulateMachine(inputs)
+        val (newMachine, _) = simulateMachine.run(initialMachine)
+
+        expectThat(newMachine).isEqualTo(Machine(true, 1, 2))
+    }
+}
