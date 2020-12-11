796d671323ec768f9dd9145957ff621fa55bf2f1
Paolo Sciarra <itbhps@gmail.com>
2020-12-11 16:44:44 +0100

    used previously created randoms and state monad map

--- src/main/kotlin/it/twinsbrains/fpik/chapter8/Generators.kt
+++ src/main/kotlin/it/twinsbrains/fpik/chapter8/Generators.kt
@@ -2,21 +2,25 @@ package it.twinsbrains.fpik.chapter8
 
 import arrow.core.Either
 import arrow.core.Tuple2
+import arrow.core.extensions.IdFunctor
 import arrow.mtl.State
 import it.twinsbrains.fpik.chapter6.RNG
+import it.twinsbrains.fpik.chapter6.Randoms.nonNegativeInt
 
 data class Gen<A>(val sample: State<RNG, A>) {
   companion object {
     fun choose(start: Int, stopExclusive: Int): Gen<Int> {
-      val s = State { rng: RNG ->
-        val (i, nRng) = rng.nextInt()
-        val r = stopExclusive - start
-        Tuple2(nRng, (i % r) + start)
-      }
+      val s = State { rng: RNG -> nonNegativeInt(rng).flip() }
+        .map(object : IdFunctor {}) { i ->
+          val r = stopExclusive - start
+          (i % r) + start
+        }
       return Gen(s)
     }
   }
 }
+
+fun <A, B> Pair<A, B>.flip(): Tuple2<B, A> = Tuple2(this.second, this.first)
 
 typealias SuccessCount = Int
 typealias FailedCase = String
