fac632744b023cbc87708f819dfeaf3927ef7b15
Paolo Sciarra <itbhps@gmail.com>
2020-12-20 17:42:58 +0100

    fork Law

--- src/test/kotlin/it/twinsbrains/fpik/chapter8/ParLawsTest.kt
+++ src/test/kotlin/it/twinsbrains/fpik/chapter8/ParLawsTest.kt
@@ -2,6 +2,7 @@ package it.twinsbrains.fpik.chapter8
 
 import it.twinsbrains.fpik.chapter7.Par
 import it.twinsbrains.fpik.chapter7.Pars.equalTo
+import it.twinsbrains.fpik.chapter7.Pars.fork
 import it.twinsbrains.fpik.chapter7.Pars.map
 import it.twinsbrains.fpik.chapter7.Pars.unit
 import it.twinsbrains.fpik.chapter8.Checkers.forAllPar
@@ -11,15 +12,24 @@ import strikt.api.expectThat
 import strikt.assertions.isA
 
 class ParLawsTest {
+  private val pint: Gen<Par<Int>> =
+    Gen.choose(0, 10).map {
+      unit(it)
+    }
+
 
   @Test
   fun mappingLaw() {
-    val pint: Gen<Par<Int>> =
-      Gen.choose(0, 10).map {
-        unit(it)
-      }
     val p = forAllPar(pint) { n: Par<Int> ->
       map(n) { it } equalTo n
+    }
+    expectThat(run(p)).isA<Passed>()
+  }
+
+  @Test
+  fun forkLaw() {
+    val p = forAllPar(pint) { n: Par<Int> ->
+      fork { n } equalTo n
     }
     expectThat(run(p)).isA<Passed>()
   }
