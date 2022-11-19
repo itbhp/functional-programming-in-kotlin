281cbfd79d7e50f575190f9f8a159f03de030132
Paolo Sciarra <itbhps@gmail.com>
2022-11-19 19:39:24 +0100

    [chapter11] introduced kleisli

--- src/main/kotlin/it/twinsbrains/fpik/chapter11/Monad.kt
+++ src/main/kotlin/it/twinsbrains/fpik/chapter11/Monad.kt
@@ -14,6 +14,15 @@ interface Monad<F> : Functor<F> {
   fun <A, B, C> map2(fa: Kind<F, A>, fb: Kind<F, B>, f: (A, B) -> C): Kind<F, C> =
     flatMap(fa) { a -> map(fb) { b -> f(a, b) } }
 
+
+  // compose kleisli arrows
+  fun <A, B, C> compose(
+    f: (A) -> Kind<F, B>,
+    g: (B) -> Kind<F, C>
+  ): (A) -> Kind<F, C> = { a: A ->
+    flatMap(f(a)) { b -> g(b) }
+  }
+
   fun <A> replicateM(n: Int, fa: Kind<F, A>): List<Kind<F, A>>
 
   fun <A> filterM(ls: List<A>, f: (A) -> Kind<F, Boolean>): Kind<F, List<A>> =
