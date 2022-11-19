ca0f639d4c5f9251b8aab3c4374c38aa59682409
Paolo Sciarra <itbhps@gmail.com>
2022-11-19 20:54:59 +0100

    [refactor] changed spec type

--- src/test/kotlin/it/twinsbrains/fpik/chapter6/NonNegativeIntsTest.kt
+++ src/test/kotlin/it/twinsbrains/fpik/chapter6/NonNegativeIntsTest.kt
@@ -1,22 +1,23 @@
 package it.twinsbrains.fpik.chapter6
 
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.WordSpec
 import io.kotest.property.forAll
 import it.twinsbrains.fpik.chapter6.Randoms.nonNegativeInt
 import it.twinsbrains.fpik.chapter6.LinearCongruentialGenerator as SimpleRNG
 
-class NonNegativeIntsTest : StringSpec() {
-    init {
-        "no elements less than 0" {
-            forAll<Long> { seed ->
-                nonNegativeInt(SimpleRNG(seed)).first >= 0
-            }
-        }
+class NonNegativeIntsTest : WordSpec({
 
-        "no elements greater than Integer.MAX_VALUE" {
-            forAll<Long> { seed ->
-                nonNegativeInt(SimpleRNG(seed)).first < Integer.MAX_VALUE
-            }
-        }
+  "NonNegativeInt" should {
+    "have no elements less than 0" {
+      forAll<Long> { seed ->
+        nonNegativeInt(SimpleRNG(seed)).first >= 0
+      }
     }
-}
+
+    "have no elements greater than Integer.MAX_VALUE" {
+      forAll<Long> { seed ->
+        nonNegativeInt(SimpleRNG(seed)).first < Integer.MAX_VALUE
+      }
+    }
+  }
+})
