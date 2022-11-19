b61b3d8510ab9bcec32b7373cde3147ee7717d80
Paolo Sciarra <itbhps@gmail.com>
2022-11-19 19:15:48 +0100

    [chapter11] defined Monad interface

---
+++ src/main/kotlin/it/twinsbrains/fpik/chapter11/Monad.kt
@@ -0,0 +1,30 @@
+package it.twinsbrains.fpik.chapter11
+
+import arrow.Kind
+
+interface Monad<F> : Functor<F> {
+
+  fun <A> unit(a: A): Kind<F, A>
+
+  fun <A, B> flatMap(fa: Kind<F, A>, f: (A) -> Kind<F, B>): Kind<F, B>
+
+  override fun <A, B> map(fa: Kind<F, A>, f: (A) -> B): Kind<F, B> =
+    flatMap(fa) { a -> unit(f(a)) }
+
+  fun <A, B, C> map2(fa: Kind<F, A>, fb: Kind<F, B>, f: (A, B) -> C): Kind<F, C> =
+    flatMap(fa) { a -> map(fb) { b -> f(a, b) } }
+
+  fun <A> replicateM(n: Int, fa: Kind<F, A>): List<Kind<F, A>>
+
+  fun <A> filterM(ls: List<A>, f: (A) -> Kind<F, Boolean>): Kind<F, List<A>> =
+    when {
+      ls.isEmpty() -> unit(listOf<A>())
+      else -> flatMap(f(ls[0])) { toKeep ->
+        if (toKeep) {
+          map(filterM(ls.subList(1, ls.size), f)) { t -> listOf(ls[0]) + t }
+        } else {
+          filterM(ls.subList(1, ls.size), f)
+        }
+      }
+    }
+}
