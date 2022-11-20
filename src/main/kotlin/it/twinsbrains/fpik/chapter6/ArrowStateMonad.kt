16ed78bbf8344455e77a0c6ed4be2436d6a783f6
Paolo Sciarra <itbhps@gmail.com>
2022-11-21 00:48:36 +0100

    [refactor] reformat

--- src/main/kotlin/it/twinsbrains/fpik/chapter6/ArrowStateMonad.kt
+++ src/main/kotlin/it/twinsbrains/fpik/chapter6/ArrowStateMonad.kt
@@ -16,25 +16,25 @@ object ArrowStateMonad {
     set(f(s)).bind()
   }
 
-    val int: State<RNG, Int> = State { rng ->
-        val (n, nRng) = rng.nextInt()
-        Tuple2(nRng, n)
-    }
+  val int: State<RNG, Int> = State { rng ->
+    val (n, nRng) = rng.nextInt()
+    Tuple2(nRng, n)
+  }
 
-    private val nonNegativeInt: State<RNG, Int> = map(int) { if (it < 0) -(it + 1) else it }
+  private val nonNegativeInt: State<RNG, Int> = map(int) { if (it < 0) -(it + 1) else it }
 
-    fun ints(count: Int): State<RNG, List<Int>> = sequence((1..count).map { nonNegativeInt })
+  fun ints(count: Int): State<RNG, List<Int>> = sequence((1..count).map { nonNegativeInt })
 
-    fun <A, B> flatMap(
-        s: State<RNG, A>,
-        f: (A) -> State<RNG, B>
-    ): State<RNG, B> = s.flatMap(object : IdMonad {}, f)
+  fun <A, B> flatMap(
+    s: State<RNG, A>,
+    f: (A) -> State<RNG, B>
+  ): State<RNG, B> = s.flatMap(object : IdMonad {}, f)
 
-    fun <A, B> map(
-        s: State<RNG, A>, f: (A) -> B
-    ): State<RNG, B> = flatMap(s) { a -> State { rng -> Tuple2(rng, f(a)) } }
+  fun <A, B> map(
+    s: State<RNG, A>, f: (A) -> B
+  ): State<RNG, B> = flatMap(s) { a -> State { rng -> Tuple2(rng, f(a)) } }
 
-    private fun <A> sequence(fs: List<State<RNG, A>>): State<RNG, List<A>> =
-        fs.stateSequential()
+  private fun <A> sequence(fs: List<State<RNG, A>>): State<RNG, List<A>> =
+    fs.stateSequential()
 
 }
