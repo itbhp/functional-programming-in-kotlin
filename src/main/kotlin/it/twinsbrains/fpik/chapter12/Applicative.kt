846ed8b1d4f8406e8a0147e0f98bf25ef685f814
Paolo Sciarra <itbhps@gmail.com>
2022-11-23 18:38:24 +0100

    [chapter12] added Applicative

---
+++ src/main/kotlin/it/twinsbrains/fpik/chapter12/Applicative.kt
@@ -0,0 +1,34 @@
+package it.twinsbrains.fpik.chapter12
+
+import arrow.Kind
+import it.twinsbrains.fpik.chapter11.Functor
+
+interface Applicative<F> : Functor<F> {
+  fun <A, B, C> map2(
+    fa: Kind<F, A>,
+    fb: Kind<F, B>,
+    f: (A, B) -> C
+  ): Kind<F, C>
+
+  fun <A> unit(a: A): Kind<F, A>
+
+  fun <A, B> traverse(
+    la: List<A>,
+    f: (A) -> Kind<F, B>
+  ): Kind<F, List<B>> =
+    la.foldRight(
+      unit(listOf())
+    ) { a: A, acc: Kind<F, List<B>> ->
+      map2(f(a), acc) { b: B, lb: List<B> -> listOf(b) + lb }
+    }
+
+  fun <A> sequence(lfa: List<Kind<F, A>>): Kind<F, List<A>> = traverse(lfa) { it }
+
+  fun <A> replicateM(n: Int, ma: Kind<F, A>): Kind<F, List<A>> =
+    sequence(List(n) { ma })
+
+  fun <A, B> product(
+    ma: Kind<F, A>,
+    mb: Kind<F, B>
+  ): Kind<F, Pair<A, B>> = map2(ma, mb) { a, b -> a to b }
+}
